ae258d6ecb3019fe348918fb5533a53d63f6c6af
Dipak76 <107499257+Dipak76@users.noreply.github.com>
2022-09-12 22:13:21 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { Route, Routes, Switch } from "react-router";
-import { Home } from "./Pages/Home";
-import { Index } from "./Pages/Index";
-import { NotFound } from "./Pages/NotFound";
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from '../AppRoutes';
+import '../styles/App.css';
+import { NavBar } from './NavBar';
+const App = () => {
 
-export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" exact element={<Index />} />
-      <Route path="/home" exact element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
+    <BrowserRouter>
+      <div id="main">
+        <NavBar />
+        <AppRoutes />
+      </div>
+    </BrowserRouter>
+
+  )
+}
+
+
+export default App;
